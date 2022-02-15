7def86cf06c157e5ff5b4d6ee4c6cfd78316457f
Dato <92759818+avangardor@users.noreply.github.com>
2022-02-16 00:23:54 +0400

    Add files via upload

---
+++ Calc.js
@@ -0,0 +1,79 @@
+const equal = document.getElementById('equal');
+const btnNum = document.querySelectorAll('[data-number]')
+const btnOper = document.querySelectorAll('[data-operator]')
+let fullNum;
+
+
+for(let el of btnNum)
+{
+    el.addEventListener('click', () => 
+    {
+        output.innerHTML += el.innerHTML
+    })
+}
+for(let el of btnOper)
+{
+    el.addEventListener('click', () => 
+    {
+        output.innerHTML += el.innerHTML
+    })
+}
+equal.addEventListener('click', () => 
+{
+    makeSum();
+    makeSubstact();
+    makeMultiply();
+    makeDivide();
+})
+
+function makeSum()
+{
+    fullNum = output.innerHTML;
+    for(let el of fullNum)
+    {
+        if(el === '+')
+        {
+            output.innerHTML = '';
+            output.innerHTML += (Number(output.innerHTML.split('+')[0])) + (Number(output.innerHTML.split('+')[1]));
+        }
+    }
+}
+function makeSubstact()
+{
+    for(let el of output.innerHTML)
+    {
+        if(el === '-')
+        {
+            output.innerHTML = '';
+            output.innerHTML += (Number(fullNum.split('-')[0])) - (Number(fullNum.split('-')[1]));
+        }
+    }
+}
+function makeMultiply()
+{
+    for(let el of output.innerHTML)
+    {
+        if(el === '*')
+        {
+            output.innerHTML = '';
+            output.innerHTML += (Number(fullNum.split('*')[0])) * (Number(fullNum.split('*')[1]));
+        }
+    }
+}
+function makeDivide()
+{
+    for(let el of output.innerHTML)
+    {
+        if(el === '/')
+        {
+            output.innerHTML = '';
+            output.innerHTML += (Number(fullNum.split('/')[0])) / (Number(fullNum.split('/')[1]));
+        }
+    }
+}
+clear.addEventListener('click', () => 
+{
+    output.innerHTML = '';
+    fullNum = '';
+})
+
